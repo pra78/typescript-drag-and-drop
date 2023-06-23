8cf49bc7a6c2ab2ace156ff35c6cb2aa72c04d5b
pra78 <66877961+pra78@users.noreply.github.com>
2023-06-23 10:49:50 +0300

    class project

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,14 @@
+// Project Type
+class Project {
+  constructor(
+    public id: string,
+    public title: string,
+    public decsription: string,
+    public people: number,
+    public status: string
+  ) {}
+}
+
 // Project State Management
 class ProjectState {
   private listeners: any[] = [];
